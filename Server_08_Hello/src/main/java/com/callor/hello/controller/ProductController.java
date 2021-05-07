466522d31c63b684a34f5b465f0bb5d82fbb65e6
Eungyeol41 <iniziato@naver.com>
2021-05-07 16:41:33 +0900

    Hello

---
+++ Server_08_Hello/src/main/java/com/callor/hello/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.callor.hello.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/product/*)")
+public class ProductController extends HttpServlet{
+
+	// 입력처리를 위한 처음 화면 보여주기
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	// 데이터를 받아서 처리하고 결과 보여주기
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	
+}
